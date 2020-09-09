97fb06a2bb1e5c721c363117678bc45b4dc2dcdc
Juan Manuel Ocampo Valencia <juano@hourly.app>
2020-09-09 17:46:53 -0500

    UI init and pod init

---
+++ Sazon/Sazon/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  Sazon
+//
+//  Created by juan ocampo on 8/24/20.
+//  Copyright © 2020 juan ocampo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
